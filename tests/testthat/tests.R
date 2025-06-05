934290e8f8589486365ae34d9757f49dae2b121c
serkor1 <77464572+serkor1@users.noreply.github.com>
2025-06-05 17:05:38 +0200

    :bug:-fix
    * Added line-ending to DESCRIPTION
    * Fixed unit-test (Were passing two scalars instead of vector)

--- tests/testthat/tests.R
+++ tests/testthat/tests.R
@@ -9,7 +9,7 @@ testthat::test_that(
 
     ## test the reference sum function
     testthat::expect_equal(
-      object   = reference_sum(1.2, 1.3),
+      object   = reference_sum(c(1.2, 1.3)),
       expected = sum(c(1.2, 1.3))
     )
     
